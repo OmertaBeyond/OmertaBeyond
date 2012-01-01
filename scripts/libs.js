9097a01b1fc106f6f96a1f0f8b2ea8c95ffc713a
vBm <the.vbm@gmail.com>
2012-01-01 18:38:01 +0000

    * updated year in headers * updated rev number in all files * replaced spaces with tabs

--- scripts/libs.js
+++ scripts/libs.js
@@ -3,7 +3,7 @@
 
 	Feel free to use them, but please let us know.
 
-	Version: 1.10.0.58
+	Version: 1.10.0.61
 
 	$Rev$:  Revision of last commit
 	$Author$:  Author of last commit
@@ -166,7 +166,6 @@ function $Cut(xpath, deep) {
 }
 
 function $I(xp, val) {
-
 	if (!val) {
 		return $X(xp).innerHTML;
 	} else {
