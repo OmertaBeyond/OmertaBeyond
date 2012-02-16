c031c9110deee95b048efe60d1f6cd78a67c5d47
vBm <the.vbm@gmail.com>
2012-02-16 18:59:09 +0000

    * Added default update meta file for stable versions  (change only when release stable)

---
+++ update/beyond.meta.js
@@ -0,0 +1,69 @@
+// ==UserScript==
+// @name			Omerta Beyond
+// @id				Omerta Beyond
+// @version			1.10.0.85
+// @date			16-02-2012
+// @author			OBDev Team <info@omertabeyond.com>
+// @author			vBm <vbm@omertabeyond.com>
+// @author			Dopedog <dopedog@omertabeyond.com>
+// @author			Rix <rix@omertabeyond.com>
+// @author			MrWhite <mrwhite@omertabeyond.com>
+// @license			GNU General Public License v3
+// @namespace			v3.omertabeyond.com
+// @homepageURL			http://www.omertabeyond.com/
+// @updateURL			https://omertabeyond.googlecode.com/svn/trunk/update/beyond.meta.js
+// @supportURL			http://helpdesk.omertabeyond.com/
+// @description			Omerta Beyond 1.10 (Still the best 'legal' script! ;))
+// @icon			http://omertabeyond.com/releases/1.10.0.85/images/logo.small.png
+// @screenshot			http://omertabeyond.com/releases/1.10.0.85/images/sshot.png http://omertabeyond.com/releases/1.10.0.85/images/sshot_tn.png
+// @encoding			UTF-8
+// @priority			1
+// @require			http://omertabeyond.com/releases/1.10.0.85/scripts/libs.js
+// @require			http://omertabeyond.com/releases/1.10.0.85/scripts/settings.js
+// @require			http://omertabeyond.com/releases/1.10.0.85/scripts/langs.js
+// @resource			css		http://omertabeyond.com/releases/1.10.0.85/scripts/beyond.css
+// @resource			trash		http://omertabeyond.com/releases/1.10.0.85/images/del.png
+// @resource			colorpicker	http://omertabeyond.com/releases/1.10.0.85/images/colorpicker.gif
+// @resource			comLogo		http://omertabeyond.com/releases/1.10.0.85/images/logo-com.png
+// @resource			dmLogo		http://omertabeyond.com/releases/1.10.0.85/images/logo-dm.png
+// @resource			nlLogo		http://omertabeyond.com/releases/1.10.0.85/images/logo-nl.png
+// @resource			buttonMenu	http://omertabeyond.com/releases/1.10.0.85/images/menu.png
+// @resource			buttonReset	http://omertabeyond.com/releases/1.10.0.85/images/reset.png
+// @resource			favoriteIco	http://omertabeyond.com/releases/1.10.0.85/images/favicon.png
+// @resource			edoIco		http://omertabeyond.com/releases/1.10.0.85/images/edo.png
+// @resource			updateIco	http://omertabeyond.com/releases/1.10.0.85/images/updateicon.png
+// @resource			brcGear		http://omertabeyond.com/releases/1.10.0.85/images/brcgear.png
+// @resource			deleteIcon	http://omertabeyond.com/releases/1.10.0.85/images/deleteicon.png
+// @resource			reply		http://omertabeyond.com/releases/1.10.0.85/images/reply.png
+// @resource			loading		http://omertabeyond.com/releases/1.10.0.85/images/loading.png
+// @resource			nickreader	http://omertabeyond.com/releases/1.10.0.85/images/magnifier.png
+// @resource			nextIcon	http://omertabeyond.com/releases/1.10.0.85/images/next.png
+// @resource			prevIcon	http://omertabeyond.com/releases/1.10.0.85/images/prev.png
+// @resource			check		http://omertabeyond.com/releases/1.10.0.85/images/check.png
+// @resource			changelog	http://omertabeyond.com/releases/1.10.0.85/images/changelog.png
+// @include			http://gm.omertabeyond.com/*.php*
+// @include			http://www.omertabeyond.com/html/poll/poll.php*
+// @include			http://www.omerta3.com/*
+// @include			http://omerta3.com/*
+// @include			http://www.barafranca.com/*
+// @include			http://barafranca.com/*
+// @include			http://www.barafranca.us/*
+// @include			http://barafranca.us/*
+// @include			http://dm.barafranca.com/*
+// @include			http://deathmatch.barafranca.com/*
+// @include			http://www.barafranca.nl/*
+// @include			http://barafranca.nl/*
+// @include			http://www.barafranca.gen.tr/*
+// @include			http://barafranca.gen.tr/*
+// @exclude			http://gamewiki.barafranca.com/*
+// @exclude			http://ircwiki.barafranca.com/*
+// @exclude			http://*barafranca.*/front-mafia-list.php*
+// @exclude			http://*barafranca.*/BeO/webroot/index.php?module=Donate.*
+// @exclude			http://*barafranca.*/game-register.php*
+// ==/UserScript==
+
+/*
+$Rev: 760 $:  Revision of last commit
+$Author: the.vbm $:  Author of last commit
+$Date: 2012-02-16 17:21:03 +0100 (Thu, 16 Feb 2012) $:  Date of last commit
+*/
